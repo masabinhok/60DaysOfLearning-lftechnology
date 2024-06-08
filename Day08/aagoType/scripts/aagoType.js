962c1a14bd368d51963029728d80afded452db6a
Sabin Shrestha <sabin.shrestha.er@gmail.com>
2024-06-08 20:53:10 +0545

    keyboard typing event

--- Day08/aagoType/scripts/aagoType.js
+++ Day08/aagoType/scripts/aagoType.js
@@ -11,27 +11,42 @@ function randomWord() {
 //default loading
 newGame();
 
+
+function addClass(el, name){
+  el.className+=' '+name;
+
+}
+
+function removeClass(el, name){
+  el.className = el.className.replace(name, '');
+}
+
 //format word
 function formatWord(word) {
   return `<div class="word">
-  <span class="letter">
-  ${word.split("").join(`</span><span class="letter">`)}</span></div>`;
+  <span class="letter">${word.split("").join(`</span><span class="letter">`)}</span></div>`;
 }
 
 //new game
 function newGame() {
   document.querySelector("#words").innerHTML = "";
   for (let i = 0; i < 200; i++) {
-    document.querySelector("#words").innerHTML += formatWord(
-      randomWord()
-    );
+    document.querySelector("#words").innerHTML += formatWord(randomWord());
   }
+  document.querySelector('.word').classList.add('current');
+  document.querySelector('.letter').classList.add('current');
 }
+
+//
+document.getElementById("game").addEventListener("keyup", (ev) => {
+  const key = ev.key;
+  const currentLetter = document.querySelector(".letter.current");
+  const expected = currentLetter.textContent;
+  console.log(key, expected);
+});
 
 //new-game-button eventlistener
 document.querySelector(".new-game").addEventListener("click", newGame);
 
 //restart-button evenlistener
 document.querySelector(".try-again").addEventListener("click", newGame);
-
-
